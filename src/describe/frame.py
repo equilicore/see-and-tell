590035f09fbd62ee8592c4e7922f96a890ea711a
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-05-06 16:45:56 +0300

    add: gpu support for describer

--- src/describe/frame.py
+++ src/describe/frame.py
@@ -1,10 +1,11 @@
 import argparse
-import logging
+import multiprocessing as mp
+import os
 import torch
 import PIL
 import numpy as np
 
-from transformers import AutoProcessor, AutoModelForCausalLM
+from transformers import AutoProcessor, AutoModelForCausalLM, PegasusForConditionalGeneration, PegasusTokenizer
 from ..log import get_pipeline_logger
 
 class FrameDescriptor:
@@ -12,6 +13,7 @@ class FrameDescriptor:
             self,
             model_name: str,
             state_dict_path: str=None,
+            use_gpu=True,
         ) -> None:
         """Initialize the FrameDescriptor class.
 
@@ -19,14 +21,48 @@ class FrameDescriptor:
             model_name (str): The name of the model to use. Must be a model from the HuggingFace model hub.
             state_dict_path (str, optional): The path to a state dict to load into the model. Defaults to None.
         """
-        self.logger = get_pipeline_logger("FrameDescriptor")
+        self.logger = get_pipeline_logger("FrameDescriptor", 'green')
         self.descriptor_model = AutoModelForCausalLM.from_pretrained(model_name)
         self.processor = AutoProcessor.from_pretrained(model_name)
 
         if state_dict_path:
             self.descriptor_model.load_state_dict(torch.load(state_dict_path))
 
+        if use_gpu and torch.cuda.is_available():
+            self.descriptor_model.cuda()
+
+        self.use_gpu = use_gpu
+
         self.logger.info(f"Initialized FrameDescriptor with model {model_name} and state_dict {state_dict_path}")
+
+    def describe_batch(self, images: list[str]) -> list[str]:
+        """Describe a batch of images.
+
+        Args:
+            images (list[str]): The list of images to describe.
+
+        Returns:
+            list[str]: The list of descriptions of the images.
+        """
+        read_images = []
+        for image in images:
+            read_images.append(
+                np.array(
+                    PIL.Image.open(image).convert("RGB")
+                )
+            )
+
+
+        self.logger.info(f"Processing batch of images {list(map(os.path.basename, images))}")
+        inputs = self.processor(images=read_images, return_tensors="pt", padding=True)
+        if self.use_gpu:
+            inputs = inputs.pixel_values.cuda()
+        else:
+            inputs = inputs.pixel_values
+        out = self.descriptor_model.generate(pixel_values=inputs, max_length=20, num_beams=5)
+        out = self.processor.batch_decode(out, skip_special_tokens=True)
+        return out
+    
 
     def __call__(self, image: torch.Tensor | str, max_description_length=40) -> str:
         """Generate a description of the image.
@@ -38,7 +74,7 @@ class FrameDescriptor:
         Returns:
             str: The description of the image.
         """
-
+        __image = "tensor_image"
         if isinstance(image, str):
             # Load the image if path is specified
             __image = "" + image
@@ -47,15 +83,14 @@ class FrameDescriptor:
                     PIL.Image.open(image).convert("RGB")
                 )
             ).permute(2, 0, 1)
-            self.logger.info(f"Loaded image from {__image}")
-        
-        self.logger.info(f"Processing image")
+                
+        self.logger.info(f"Processing image {__image}")
         inputs = self.processor(images=image, return_tensors="pt")
-        out = self.descriptor_model.generate(pixel_values=inputs.pixel_values, max_new_tokens=max_description_length)
+        out = self.descriptor_model.generate(pixel_values=inputs.pixel_values, max_length=max_description_length, num_beams=5)
         out = self.processor.batch_decode(out, skip_special_tokens=True)[0]
-        self.logger.info(f"Processed. Generated description: {out}...")
-
+        self.logger.info(f"Processed {__image}. Generated description: {out}")
         return out
+    
             
         
 
@@ -63,10 +98,20 @@ if __name__ == "__main__":
     # Example usage
     argparser = argparse.ArgumentParser()
     argparser.add_argument("path", type=str, help="The path to the image to describe.")
+    argparser.add_argument("--use-dir", action="store_true", help="Whether to use the path as a directory.")
     args = argparser.parse_args()
 
     frame_desc = FrameDescriptor(
-        model_name="microsoft/git-base-textcaps"
+        model_name="microsoft/git-base-textcaps",
+        use_gpu=True
     )
 
-    frame_desc(args.path)
+    if args.use_dir:
+        # Describe all images in the directory
+        images = os.listdir(args.path)
+        images = list(map(lambda x: os.path.join(args.path, x), images))
+        descriptions = frame_desc.describe_batch(images)
+        for image, description in zip(images, descriptions):
+            print(f"Image {image} has description {description}")
+    else:
+        frame_desc(args.path)
