8d74c222e11aedb6c52556d6bc5ac72de1898cd1
ayhem bouabid <bouabidayhem@gmail.com>
2023-07-18 12:32:48 +0100

    fixing imports + returning the indices of the captions used

--- src/captions/captions_improved.py
+++ src/captions/captions_improved.py
@@ -131,7 +131,7 @@ def map_predictions(noun_phrases: list[list[str]],
                     decided_prediction_np: dict[str, Counter]) -> dict[str: list[str]]:
 
     # since it is not possible to have lists as indices, we need to convert noun phrases (as lists) to strings
-    # we need a mapping from the resultings strings to the original lists
+    # we need a mapping from the resulting strings to the original lists
     def to_str(tokens: list[str]) -> str:
         return (" ".join(tokens)).lower().strip()
 
