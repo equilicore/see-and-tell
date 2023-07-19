92df24b742a5898266c5eb4fa2431231f4e723fb
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-07-19 12:30:56 +0500

    Pre-release: small fixes

--- src/cntell.py
+++ src/cntell.py
@@ -26,11 +26,9 @@ def main():
         description="See and Tell: A tool to describe videos using speech and image recognition.\n\n"
         "To run a pipeline you need to specify the path to the video to describe and the "
         "path to save the output video to. Pipeline uses temporary files to store intermediate "
-        "results, you can specify the path to save them to using the --temp argument. You can "
-        "also specify the number of cpus to use using the --cpus argument. The --serie argument "
+        "results, you can specify the path to save them to using the --temp argument. The --serie argument "
         "specifies the serie to recognize characters from. The --embeddings argument specifies "
         "the folder to search for embeddings for the serie in."
-        
     )
     argparser.add_argument("video", help="The path to the video to describe.")
     argparser.add_argument("output", help="The path to save the output video to.")
