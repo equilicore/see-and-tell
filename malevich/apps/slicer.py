2c66e7f36f3dbb59ffd5b352a93554dbed91d68d
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-09-09 17:41:16 +0300

    upd: Malevich-friendly Slicer is ready

--- malevich/apps/slicer.py
+++ malevich/apps/slicer.py
@@ -8,22 +8,52 @@ from typing import Any
 
 @jls.processor()
 def slice_video(files: DF[Any], context: Context):
-    save_dir = os.path.join(APP_DIR, context.app_cfg.get('save_dir', 'frames'))
-    slicer = Slicer(
-        save_dir=save_dir
-    )
+    save_dir = context.app_cfg.get('save_dir', 'sliced')
+    # Real saving dir is APP_DIR/save_dir
+    # there files will exist, but the share path will be
+    # save_dir/run_id
+    real_save_dir = os.path.join(APP_DIR, save_dir)
+    os.makedirs(real_save_dir, exist_ok=True)
     
-    out: list[Slicer.FramesAndAudio] = []
+    
+    # Will save
+    # APP_DIR/save_dir/*.png
+    # APP_DIR/save_dir/audio.wav
+    slicer = Slicer(
+        save_dir=real_save_dir,
+        naming='f_%10d'
+    )    
+
+    
+    frame_paths = []
+    
     for path in files.get('filename'):
         real_path = context.get_share_path(path)
-        out.append(
-            slicer.run(Slicer.VideoPath(video_path=real_path))  
-        )
+        _ = slicer.run(Slicer.VideoPath(video_path=real_path))  
         
-    return pd.DataFrame({
-        'filenames': sum(map(lambda x: x.frames_path, out), []),
-        'audio': list(map(lambda x: x.audio_path, out))
-    })
+    frame_paths = [os.path.join(save_dir, x) for x in os.listdir(real_save_dir) if x.endswith('.png')]
+    audio_path = os.path.join(save_dir, 'audio.wav')
+    
+    
+    return_frame_paths = []
+    
+    for pth in frame_paths:
+        context.share(pth)
+        return_frame_paths.append(pth)
+        
+    context.share(audio_path)       
+    return_audio_path = [audio_path]
+     
+        
+        
+    return [ 
+        pd.DataFrame({
+            'filenames': return_frame_paths
+        }),
+        pd.DataFrame({
+            'audio': return_audio_path
+        })
+    ]
         
     
         
